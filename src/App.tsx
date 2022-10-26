9e6a0f153fb60d9a49478e603864961647a320b8
Eber Martins Rodrigues <eberjoe@gmail.com>
2022-10-25 23:23:41 -0300

    Update home

--- src/App.tsx
+++ src/App.tsx
@@ -11,40 +11,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="header">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
-        </p>
+        <p className="header">🚀 Turning Copper Work in Progress</p>
 
         <div className="body">
           <button onClick={() => setCount((count) => count + 1)}>
             🪂 Click me : {count}
           </button>
 
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
+          <p>New things will be turning briefly! Keep tuned!</p>
         </div>
       </header>
     </div>
